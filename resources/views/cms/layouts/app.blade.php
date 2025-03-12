0547a0a0bec0647672e578ced8ddb9227ecbc362
salwaahmadz <salwa.ahmad@lumoshive.com>
2025-03-12 12:41:34 +0700

    fix asset link 2

--- resources/views/cms/layouts/app.blade.php
+++ resources/views/cms/layouts/app.blade.php
@@ -5,7 +5,7 @@
   class="light-style layout-menu-fixed"
   dir="ltr"
   data-theme="theme-default"
-  data-assets-path="{{ asset('/assets') }}"
+  data-assets-path="{{ asset('assets') }}"
   data-template="vertical-menu-template-free"
 >
   <head>
--- resources/views/cms/layouts/datatables_css.blade.php
+++ resources/views/cms/layouts/datatables_css.blade.php
@@ -1,2 +1,2 @@
 {{-- <link rel="stylesheet" href="https://cdn.datatables.net/2.1.2/css/dataTables.dataTables.css" /> --}}
-<link rel="stylesheet" href="{{ asset('/assets/css/dataTables.css') }}" />
+<link rel="stylesheet" href="{{ asset('assets/css/dataTables.css') }}" />
